19cc5134fccb679e9e1697d6a26931e6fc612cab
irtfrm <frm.tomoya@gmail.com>
2021-06-03 01:59:08 +0900

    11

--- 11.container-with-most-water.py
+++ 11.container-with-most-water.py
@@ -13,20 +13,15 @@ class Solution(object):
         :rtype: int
         """
         M = 0
-        height_sorted = [(i,height[i]) for i in range(len(height))]
-        height_sorted = sorted(height_sorted, key=lambda x:x[1])
 
         left = 0
         right = len(height)-1
-        for x in height_sorted:
-            target_i = x[0]
-            if target_i-left > right-target_i:
-                M = max((target_i - left)*height[target_i], M)
-            elif target_i-left < right-target_i:
-                M = max((right - target_i)*height[target_i], M)
-            while left < right and height[left] <= height[target_i]:
+        while left < right:
+            min_height = min(height[left], height[right])
+            M = max((right - left)*min_height, M)
+            while left < right and height[left] <= min_height:
                 left += 1
-            while left < right and height[right] <= height[target_i]:
+            while left < right and height[right] <= min_height:
                 right -= 1
         return M
 # @lc code=end
