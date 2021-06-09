5012a95a0fb212708c23ff9d6f5ba40530912e57
irtfrm <frm.tomoya@gmail.com>
2021-06-09 22:27:17 +0900

    110

---
+++ 110.balanced-binary-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=110 lang=python3
+#
+# [110] Balanced Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getHeight(self, root: TreeNode):
+        if root == None:
+            return (0, True)
+        left = self.getHeight(root.left)
+        right = self.getHeight(root.right)
+
+        return (
+            max(left[0], right[0]) + 1,
+            left[1] and right[1] and abs(left[0]-right[0])<=1
+        )
+    def isBalanced(self, root: TreeNode) -> bool:
+        if root == None:
+            return True
+        return self.getHeight(root)[1]
+# @lc code=end
+
