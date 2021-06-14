4977282ef82a54e994afcdfb3625d775de998e5f
irtfrm <frm.tomoya@gmail.com>
2021-06-14 22:41:15 +0900

    144 iterative

--- 144.binary-tree-preorder-traversal.py
+++ 144.binary-tree-preorder-traversal.py
@@ -13,6 +13,18 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
+        ans = []
+        stack = [root]
+        while len(stack) != 0:
+            node = stack.pop()
+            if node != None:
+                ans.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+
+        return ans
+
+    def preorderTraversal2(self, root: TreeNode) -> List[int]:
         if root == None:
             return []
         ans = []
@@ -20,5 +32,6 @@ class Solution:
         ans += self.preorderTraversal(root.left)
         ans += self.preorderTraversal(root.right)
         return ans
+
 # @lc code=end
 
