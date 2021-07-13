6020449d62fccb81deea2c53c0bb597e5be3dd7a
irtfrm <frm.tomoya@gmail.com>
2021-07-13 21:34:13 +0900

    231

---
+++ 231.power-of-two.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=231 lang=python3
+#
+# [231] Power of Two
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n < 1:
+            return False
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            n //= 2
+        return True
+
+# @lc code=end
+
