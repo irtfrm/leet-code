b82e8e3e1897acbc24fe1575d5aff9b0acf2207d
irtfrm <frm.tomoya@gmail.com>
2021-07-26 23:24:57 +0900

    268

---
+++ 268.missing-number.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        length = len(nums)
+        return length * (length + 1) // 2 - sum(nums)
+# @lc code=end
+
