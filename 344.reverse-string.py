baf583b110f983db3b678eec0ff05f6504bfe9cb
irtfrm <frm.tomoya@gmail.com>
2021-08-05 23:17:19 +0900

    344

---
+++ 344.reverse-string.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=344 lang=python3
+#
+# [344] Reverse String
+#
+
+# @lc code=start
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
+
+# @lc code=end
+
