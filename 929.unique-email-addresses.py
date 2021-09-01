9ebc017122ea8eb1f3498e26da5b2f676fc530c5
irtfrm <frm.tomoya@gmail.com>
2021-09-01 22:50:07 +0900

    929

---
+++ 929.unique-email-addresses.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=929 lang=python3
+#
+# [929] Unique Email Addresses
+#
+
+# @lc code=start
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        s = set()
+        for email in emails:
+            local, host = email.split('@')
+            local = local.split('+')[0]
+            local = local.replace('.', '')
+            s.add(local + host)
+        return len(s)
+# @lc code=end
+
