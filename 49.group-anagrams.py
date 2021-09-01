4f6e5063f9d024bea84594a9e5b304ec66650218
irtfrm <frm.tomoya@gmail.com>
2021-09-01 22:33:11 +0900

    49

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -16,12 +16,7 @@ class Solution:
             dct[str_new].append(str)
             strs_new.append(str_new)
         strs_new.sort()
-        ans = []
-        for i, str in enumerate(strs_new):
-            if i > 0 and strs_new[i - 1] == str:
-                continue
-            ans.append(dct[str])
-        return ans
+        return dct.values()
 
 
 # @lc code=end
